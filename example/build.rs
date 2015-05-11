01f90b5d134c5d72ae574b282674ac77b5e09c25
papamitra <papamitra@gmail.com>
2015-05-11 16:47:53 +0900

    build.rs: env WAYLAND_PREFIX specify path to wayland.xml and wayland-client.h

--- example/build.rs
+++ example/build.rs
@@ -1,19 +1,25 @@
+#![feature(collections)]
 
 use std::process::Command;
 use std::env;
+use std::string::String;
 
 fn main() {
-    let out_dir = env::var("OUT_DIR").unwrap();
+    //let out_dir = env::var("OUT_DIR").unwrap();
     let top_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let wl_prefix = match env::var("WAYLAND_PREFIX") {
+        Ok(val) => val,
+        Err(_) => String::from_str("/usr")
+    };
 
     Command::new("python3").arg(&format!("{}/../scanner.py", top_dir))
         .arg(&"-o")
         .arg(&format!("{}/src/wayland_client_protocol.rs", top_dir))
-        .arg(&"/usr/local/share/wayland/wayland.xml")
+        .arg(&format!("{}/share/wayland/wayland.xml", wl_prefix))
         .status().unwrap();
 
     Command::new("bindgen").args(&["-builtins", "-o"])
         .arg(&format!("{}/src/wayland_client.rs", top_dir))
-        .arg(&"/usr/local/include/wayland-client.h")
+        .arg(&format!("{}/include/wayland-client.h", wl_prefix))
         .status().unwrap();
 }
